fca2efca1e68d7e07c89a277f0e9479b4ad8ea3f
rayveneja <rayvenejenningsallen@gmail.com>
2024-02-28 23:21:02 -0500

    Lab3 completed

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from app import app
+from app import app, mail
 from flask import render_template, request, redirect, url_for, flash
-
+from .forms import ContactForm
+from flask_mail import Message
 
 ###
 # Routing for your application.
@@ -17,6 +18,24 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+        
+        msg = Message(subject, sender=email, recipients=[app.config['MAIL_USERNAME']])
+        msg.body = f"From: {name} <{email}>\n\n{message}"
+        mail.send(msg)
+        
+        flash('Your email has been sent successfully!', 'success')
+        
+        return redirect(url_for('home'))
+    
+    return render_template('contact.html', form=form)
 
 ###
 # The functions below should be applicable to all Flask apps.
